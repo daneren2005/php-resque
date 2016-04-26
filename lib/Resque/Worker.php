05411523aebd2583c2f37eaf01cc4da60f0678f3
Scott Jackson <daneren2005@gmail.com>
2016-04-25 17:13:36 -0700

    Debug kill success

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -437,6 +437,7 @@ class Resque_Worker
 		if(exec('ps -o pid,state -p ' . $this->child, $output, $returnCode) && $returnCode != 1) {*/
 			$this->logger->log(Psr\Log\LogLevel::DEBUG, 'Child {child} found, killing.', array('child' => $this->child));
 			posix_kill($this->child, SIGKILL);
+			$this->logger->log(Psr\Log\LogLevel::DEBUG, 'Child {child} killed.', array('child' => $this->child));
 			$this->child = null;
 			
 			// Update job
